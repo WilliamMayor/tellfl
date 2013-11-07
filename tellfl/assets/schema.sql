abe01ff16191b90798f9af9258bd3c2e0f414217
William Mayor <mail@williammayor.co.uk>
2013-11-07 09:30:07 +0000

    added unique constraints

--- tellfl/assets/schema.sql
+++ tellfl/assets/schema.sql
@@ -3,8 +3,9 @@ DROP TABLE IF EXISTS journeys;
 DROP TABLE IF EXISTS payments;
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
-    username TEXT UNIQUE,
-    email TEXT
+    username TEXT,
+    email TEXT,
+    UNIQUE(username) ON CONFLICT FAIL
 );
 CREATE TABLE journeys (
     user INTEGER,
@@ -13,11 +14,13 @@ CREATE TABLE journeys (
     time_in INTEGER,
     time_out INTEGER,
     cost INTEGER,
+    UNIQUE(user, time_in) ON CONFLICT IGNORE,
     FOREIGN KEY (user) REFERENCES users(id)
 );
 CREATE TABLE payments (
     user INTEGER,
     amount INTEGER,
     time INTEGER,
+    UNIQUE(user, time) ON CONFLICT IGNORE,
     FOREIGN KEY (user) REFERENCES users(id)
 );
