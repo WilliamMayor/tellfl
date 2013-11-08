20abf1dfbdfdd79614d70ae0f75fd6066af644cc
William Mayor <mail@williammayor.co.uk>
2013-11-08 18:31:15 +0000

    Removed dropbox stuff

--- .travis/after_script.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-curl 'https://raw.github.com/WilliamMayor/Dropbox-Uploader/master/dropbox_uploader.sh' -o dropbox_uploader.sh
-chmod +x dropbox_uploader.sh
-./dropbox_uploader.sh delete /tellfl
-./dropbox_uploader.sh upload . /
