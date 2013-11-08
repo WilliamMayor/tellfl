877804c9ed0e064d6885c3eebd52da5fc141bd44
William Mayor <mail@williammayor.co.uk>
2013-11-08 13:39:44 +0000

    trying to delete dropbox repo beofre update

--- .travis/after_script.sh
+++ .travis/after_script.sh
@@ -3,4 +3,5 @@ set -e
 
 curl 'https://raw.github.com/WilliamMayor/Dropbox-Uploader/master/dropbox_uploader.sh' -o dropbox_uploader.sh
 chmod +x dropbox_uploader.sh
+./dropbox_uploader.sh delete /tellfl
 ./dropbox_uploader.sh upload . /
