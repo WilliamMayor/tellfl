ca9e074f5a60ea0e456bde7f57ac61c67b68451f
William Mayor <mail@williammayor.co.uk>
2013-11-08 12:51:19 +0000

    moved dropbox sync to after script

---
+++ .travis/after_script.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
+curl 'https://raw.github.com/WilliamMayor/Dropbox-Uploader/master/dropbox_uploader.sh' -o dropbox_uploader.sh
+chmod +x dropbox_uploader.sh
+./dropbox_uploader.sh upload . /
--- .travis/script.sh
+++ .travis/script.sh
@@ -2,6 +2,3 @@
 set -e
 
 nosetests
-curl 'https://raw.github.com/WilliamMayor/Dropbox-Uploader/master/dropbox_uploader.sh' -o dropbox_uploader.sh
-chmod +x dropbox_uploader.sh
-./dropbox_uploader.sh upload . /
